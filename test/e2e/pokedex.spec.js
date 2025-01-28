17097a11181d4da4fe1af48c84a46391d725e231
jburn <jopbruun@gmail.com>
2025-01-28 19:06:55 +0200

    11.9 finish

--- test/e2e/pokedex.spec.js
+++ test/e2e/pokedex.spec.js
@@ -3,9 +3,14 @@ const { test, describe, expect } = require('@playwright/test')
 
 
 describe('Pokedex', () => {
-  test('front page can be opened', async ({ page }) => {
+  test('Front page can be opened', async ({ page }) => {
     await page.goto('/')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+
+  test('Pokemon page can be opened', async ({ page }) => {
+    await page.goto('/pokemon/ivysaur')
+    await expect(page.getByText('chlorophyll')).toBeVisible()
   })
 })
