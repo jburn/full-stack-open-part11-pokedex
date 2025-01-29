57ce0330f57ae01f251396b3f447b33d4d2c0cef
jburn <jopbruun@gmail.com>
2025-01-29 10:07:41 +0200

    11.12 v4

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('11.12 v3')
+  res.send('11.12 v4')
 })
 
 app.get('/health', (req, res) => {
