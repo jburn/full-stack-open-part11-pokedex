3748882635fde41a782a57cb8384f828a746da71
jburn <jopbruun@gmail.com>
2025-01-29 10:15:24 +0200

    11.12 done

--- app.js
+++ app.js
@@ -8,12 +8,10 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('11.12 v4')
+  res.send('11.12 v5')
 })
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
   res.send('ok')
 })
 
