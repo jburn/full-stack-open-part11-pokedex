35a8efe4840d8bf9faf590be9cce4d4ae587502a
jburn <jopbruun@gmail.com>
2025-01-29 09:41:28 +0200

    11.11

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const express = require('express')
 const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
+
+app.get('/version', (req, res) => {
+  res.send('v1')
+})
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
